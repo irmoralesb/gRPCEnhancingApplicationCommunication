73b5916c0adb4705a775af14fe8c4104e332390c
Raul Morales <irmoralesb@hotmail.com>
2020-04-10 13:37:22 -0500

    Code Commit. This code version fails to connect, error:  "DNS resolution failed" After some research seems that there may be two issues: 1) The latest versions of gRPC are focused to .Net Core, so it may be an error for .Net Framework 2) Networking issues, such as a firewall
    However there is a post which says that moving the projects to .Net Core will work.

---
+++ src/generate_protoc.bat
@@ -0,0 +1,1 @@
+Grpc\packages\Grpc.Tools.2.28.1\tools\windows_x64\protoc.exe -I pb\ --csharp_out "Grpc\Messages" "pb\messages.proto" --grpc_out "Grpc\Messages" --plugin=protoc-gen-grpc="Grpc\packages\Grpc.Tools.2.28.1\tools\windows_x64\grpc_csharp_plugin.exe"
